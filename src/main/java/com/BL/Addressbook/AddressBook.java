adf7946543c3df341a0f4d648f2a2381eaa4701c
unknown <navyas211@gmail.com>
2021-11-15 22:12:49 +0530

    [NAVAYA]ADDED: To edit existing contact person their name

--- src/main/java/com/BL/Addressbook/AddressBook.java
+++ src/main/java/com/BL/Addressbook/AddressBook.java
@@ -9,8 +9,8 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class AddressBook {   
-	static List<ContactPerson> contactList = new ArrayList<ContactPerson>();
+public class AddressBook {    
+	static List<ContactPerson> contactList = new ArrayList<>();
 	static Scanner sc = new Scanner(System.in);
 
 	// method for adding contacts in list.
@@ -34,26 +34,19 @@ public class AddressBook {
 		contactList.add(addressBook);
 	}
 
-	// main Method
-	public static void main(String[] args) {
-		Scanner obj = new Scanner(System.in);
-		int flag = 1;
-		while (flag == 1) {
-			System.out.println(" Choose your choice : 1.Add 2.Exit ");
-			int choice = obj.nextInt();
-			switch (choice) {
-			case 1:
-				addContact();
-				break;
-			case 2:
-				flag = 0;
-				break;
-			default:
-				System.out.println("Enter a valid choice");
-				break;
+	// method for editing existing contact
+	public void editContact() {
+		Scanner nameInput = new Scanner(System.in);
+		System.out.println(" Enter the first name ");
+		String fName = nameInput.nextLine();
+		for (int index = 0; index < contactList.size(); index++) {
+			if (contactList.get(index).getFirstName().equals(fName)) {
+				contactList.remove(index);
+				AddressBook addressBook = new AddressBook();
+				addressBook.addContact();
+			} else {
+				System.out.println(" There is no contact ");
 			}
 		}
-
-		System.out.println(contactList);
 	}
 }
